66dfdc0cbf4a53dea22a8d62472c2c9034eec0ad
anthony <anthony1949@zto.com>
2023-11-28 16:20:50 +0800

    feature：添加校验工具类，可以简化使用，可以自定义异常使用 feature：枚举工具类优化

--- armguard-common/src/main/java/vip/openpark/armguard/common/util/AssertUtils.java
+++ armguard-common/src/main/java/vip/openpark/armguard/common/util/AssertUtils.java
@@ -2,185 +2,498 @@ package vip.openpark.armguard.common.util;
 
 import java.util.Collection;
 import java.util.Map;
+import java.util.function.Supplier;
 
 /**
  * @author anthony
- * @version 2023/9/6
+ * @version 2023/11/28 15:31:19
  */
 public class AssertUtils {
 
-    private AssertUtils() {
-    }
+	private AssertUtils() {
+	}
 
-    /**
-     * 抛出异常
-     * - 如有必要，可将 IllegalArgumentException 换成自定义的异常
-     *
-     * @param message 提示信息
-     */
-    public static void throwIAE(final String message) {
-        throw new IllegalArgumentException(message);
-    }
+	/**
+	 * 抛出异常
+	 * - 如有必要，可将 IllegalArgumentException 换成自定义的异常
+	 *
+	 * @param message 提示信息
+	 */
+	public static void throwIAE(final String message) {
+		throw new IllegalArgumentException(message);
+	}
 
-    /**
-     * 抛出异常
-     * - 如有必要，可将 NullPointerException 换成自定义的异常
-     *
-     * @param message 提示信息
-     */
-    public static void throwNPE(final String message) {
-        throw new NullPointerException(message);
-    }
+	/**
+	 * 抛出异常
+	 * - 如有必要，可将 NullPointerException 换成自定义的异常
+	 *
+	 * @param message 提示信息
+	 */
+	public static void throwNPE(final String message) {
+		throw new NullPointerException(message);
+	}
 
-    /**
-     * 对象类型不能为空，否则抛出异常
-     *
-     * @param obj     目标对象
-     * @param message 提示信息
-     * @param <T>     泛型
-     */
-    public static <T> void notNull(final T obj, final String message) {
-        if (ObjectUtils.isNull(obj)) {
-            throwNPE(message);
-        }
-    }
 
-    /**
-     * 对象类型必须为空，否则抛出异常
-     *
-     * @param obj     目标对象
-     * @param message 提示信息
-     * @param <T>     泛型
-     */
-    public static <T> void isNull(final T obj, final String message) {
-        if (ObjectUtils.notNull(obj)) {
-            throwIAE(message);
-        }
-    }
+	/**
+	 * 表达式结果必须为 true，否则抛出异常
+	 *
+	 * @param expression 目标表达式结果
+	 */
+	public static void isTrue(final boolean expression) {
+		if (!expression) {
+			throwIAE("The result of this expression is false, but it is required to be true.");
+		}
+	}
 
-    /**
-     * 字符串类型不能为空，否则抛出异常
-     *
-     * @param chars   目标对象
-     * @param message 提示信息
-     */
-    public static <T extends CharSequence> void notBlank(final T chars, final String message) {
-        if (StringUtils.isBlank(chars)) {
-            throwIAE(message);
-        }
-    }
+	/**
+	 * 表达式结果必须为 true，否则抛出异常
+	 *
+	 * @param expression 目标表达式结果
+	 * @param message    提示信息
+	 */
+	public static void isTrue(final boolean expression, final String message) {
+		if (!expression) {
+			throwIAE(message);
+		}
+	}
 
-    /**
-     * 字符串类型为空或者 null，否则抛出异常
-     *
-     * @param chars   目标对象
-     * @param message 提示信息
-     */
-    public static <T extends CharSequence> void isBlank(final T chars, final String message) {
-        if (StringUtils.isNotBlank(chars)) {
-            throwIAE(message);
-        }
-    }
+	/**
+	 * 表达式结果必须为 true，否则抛出异常
+	 *
+	 * @param expression 目标表达式结果
+	 * @param supplier   EX Supplier
+	 * @param <EX>       泛型
+	 */
+	public static <EX extends RuntimeException> void isTrue(final boolean expression, final Supplier<EX> supplier) {
+		if (!expression) {
+			throw supplier.get();
+		}
+	}
 
-    /**
-     * 表达式结果必须为 true，否则抛出异常
-     *
-     * @param expression 目标表达式结果
-     * @param message    提示信息
-     */
-    public static void isTrue(final boolean expression, final String message) {
-        if (!expression) {
-            throwIAE(message);
-        }
-    }
+	/**
+	 * 表达式结果必须为 false，否则抛出异常
+	 *
+	 * @param expression 目标表达式结果
+	 */
+	public static void isFalse(final boolean expression) {
+		if (expression) {
+			throwIAE("The result of this expression is true, but it is required to be false.");
+		}
+	}
 
-    /**
-     * 表达式结果必须为 false，否则抛出异常
-     *
-     * @param expression 目标表达式结果
-     * @param message    提示信息
-     */
-    public static void isFalse(final boolean expression, final String message) {
-        if (expression) {
-            throwIAE(message);
-        }
-    }
+	/**
+	 * 表达式结果必须为 false，否则抛出异常
+	 *
+	 * @param expression 目标表达式结果
+	 * @param message    提示信息
+	 */
+	public static void isFalse(final boolean expression, final String message) {
+		if (expression) {
+			throwIAE(message);
+		}
+	}
 
-    /**
-     * 数组不能为空，否则抛出异常
-     *
-     * @param obj     目标对象
-     * @param message 提示信息
-     * @param <T>     泛型
-     */
-    public static <T> void notEmpty(final T[] obj, final String message) {
-        if (CollectionUtils.isEmpty(obj)) {
-            throwIAE(message);
-        }
-    }
+	/**
+	 * 表达式结果必须为 false，否则抛出异常
+	 *
+	 * @param expression 目标表达式结果
+	 * @param supplier   EX Supplier
+	 * @param <EX>       泛型
+	 */
+	public static <EX extends RuntimeException> void isFalse(final boolean expression, final Supplier<EX> supplier) {
+		if (expression) {
+			throw supplier.get();
+		}
+	}
 
-    /**
-     * 数组必须为空，否则抛出异常
-     *
-     * @param obj     目标对象
-     * @param message 提示信息
-     * @param <T>     泛型
-     */
-    public static <T> void isEmpty(final T[] obj, final String message) {
-        if (CollectionUtils.nonEmpty(obj)) {
-            throwIAE(message);
-        }
-    }
 
-    /**
-     * 集合不能为空，否则抛出异常
-     *
-     * @param collection 目标集合
-     * @param message    提示信息
-     * @param <T>        泛型
-     */
-    public static <T extends Collection<?>> void notEmpty(final T collection, final String message) {
-        if (CollectionUtils.isEmpty(collection)) {
-            throwIAE(message);
-        }
-    }
+	/**
+	 * 对象类型不能为空，否则抛出异常
+	 *
+	 * @param obj 目标对象
+	 * @param <T> 泛型
+	 */
+	public static <T> void notNull(final T obj) {
+		if (ObjectUtils.isNull(obj)) {
+			throwNPE("The object is null, but it is required not null.");
+		}
+	}
 
-    /**
-     * 集合必须为空或者 null，否则抛出异常
-     *
-     * @param collection 目标集合
-     * @param message    提示信息
-     * @param <T>        泛型
-     */
-    public static <T extends Collection<?>> void isEmpty(final T collection, final String message) {
-        if (CollectionUtils.nonEmpty(collection)) {
-            throwIAE(message);
-        }
-    }
+	/**
+	 * 对象类型不能为空，否则抛出异常
+	 *
+	 * @param obj     目标对象
+	 * @param message 提示信息
+	 * @param <T>     泛型
+	 */
+	public static <T> void notNull(final T obj, final String message) {
+		if (ObjectUtils.isNull(obj)) {
+			throwNPE(message);
+		}
+	}
 
-    /**
-     * map集合不能为空，否则抛出异常
-     *
-     * @param map     目标集合
-     * @param message 提示信息
-     * @param <T>     泛型
-     */
-    public static <T extends Map<?, ?>> void notEmpty(final T map, final String message) {
-        if (MapUtils.isEmpty(map)) {
-            throwIAE(message);
-        }
-    }
+	/**
+	 * 对象类型不能为空，否则抛出异常
+	 *
+	 * @param obj      目标对象
+	 * @param supplier EX supplier
+	 * @param <T>      泛型
+	 * @param <EX>     泛型
+	 */
+	public static <T, EX extends RuntimeException> void notNull(final T obj, final Supplier<EX> supplier) {
+		if (ObjectUtils.isNull(obj)) {
+			throw supplier.get();
+		}
+	}
 
-    /**
-     * map集合必须为空，否则抛出异常
-     *
-     * @param map     目标集合
-     * @param message 提示信息
-     * @param <T>     泛型
-     */
-    public static <T extends Map<?, ?>> void isEmpty(final T map, final String message) {
-        if (MapUtils.nonEmpty(map)) {
-            throwIAE(message);
-        }
-    }
+	/**
+	 * 对象类型必须为空，否则抛出异常
+	 *
+	 * @param obj 目标对象
+	 * @param <T> 泛型
+	 */
+	public static <T> void isNull(final T obj) {
+		if (ObjectUtils.notNull(obj)) {
+			throwIAE("The object is not null, but it is required null.");
+		}
+	}
+
+	/**
+	 * 对象类型必须为空，否则抛出异常
+	 *
+	 * @param obj     目标对象
+	 * @param message 提示信息
+	 * @param <T>     泛型
+	 */
+	public static <T> void isNull(final T obj, final String message) {
+		if (ObjectUtils.notNull(obj)) {
+			throwIAE(message);
+		}
+	}
+
+	/**
+	 * 对象类型必须为空，否则抛出异常
+	 *
+	 * @param obj      目标对象
+	 * @param supplier EX supplier
+	 * @param <T>      泛型
+	 * @param <EX>     泛型
+	 */
+	public static <T, EX extends RuntimeException> void isNull(final T obj, final Supplier<EX> supplier) {
+		if (ObjectUtils.notNull(obj)) {
+			throw supplier.get();
+		}
+	}
+
+	/**
+	 * 字符串类型不能为空，否则抛出异常
+	 *
+	 * @param chars 目标对象
+	 * @param <T>   泛型
+	 */
+	public static <T extends CharSequence> void notBlank(final T chars) {
+		if (StringUtils.isBlank(chars)) {
+			throwIAE("The chars is bank , but it is required not bank.");
+		}
+	}
+
+	/**
+	 * 字符串类型不能为空，否则抛出异常
+	 *
+	 * @param chars   目标对象
+	 * @param message 提示信息
+	 */
+	public static <T extends CharSequence> void notBlank(final T chars, final String message) {
+		if (StringUtils.isBlank(chars)) {
+			throwIAE(message);
+		}
+	}
+
+	/**
+	 * 字符串类型不能为空，否则抛出异常
+	 *
+	 * @param chars    目标对象
+	 * @param supplier EX Supplier
+	 * @param <T>      泛型
+	 * @param <EX>     泛型
+	 */
+	public static <T extends CharSequence, EX extends RuntimeException> void notBlank(final T chars, final Supplier<EX> supplier) {
+		if (StringUtils.isBlank(chars)) {
+			throw supplier.get();
+		}
+	}
+
+	/**
+	 * 字符串类型为空或者 null，否则抛出异常
+	 *
+	 * @param chars 目标对象
+	 * @param <T>   泛型
+	 */
+	public static <T extends CharSequence> void isBlank(final T chars) {
+		if (StringUtils.isNotBlank(chars)) {
+			throwIAE("The chars is not bank , but it is required bank.");
+		}
+	}
+
+	/**
+	 * 字符串类型为空或者 null，否则抛出异常
+	 *
+	 * @param chars   目标对象
+	 * @param message 提示信息
+	 */
+	public static <T extends CharSequence> void isBlank(final T chars, final String message) {
+		if (StringUtils.isNotBlank(chars)) {
+			throwIAE(message);
+		}
+	}
+
+	/**
+	 * 字符串类型为空或者 null，否则抛出异常
+	 *
+	 * @param chars    目标对象
+	 * @param supplier EX Supplier
+	 * @param <T>      泛型
+	 * @param <EX>     泛型
+	 */
+	public static <T extends CharSequence, EX extends RuntimeException> void isBlank(final T chars, final Supplier<EX> supplier) {
+		if (StringUtils.isNotBlank(chars)) {
+			throw supplier.get();
+		}
+	}
+
+	/**
+	 * 数组不能为空，否则抛出异常
+	 *
+	 * @param obj 目标对象
+	 * @param <T> 泛型
+	 */
+	public static <T> void notEmpty(final T[] obj) {
+		if (CollectionUtils.isEmpty(obj)) {
+			throwIAE("The array is empty , but it is required not empty.");
+		}
+	}
+
+	/**
+	 * 数组不能为空，否则抛出异常
+	 *
+	 * @param obj     目标对象
+	 * @param message 提示信息
+	 * @param <T>     泛型
+	 */
+	public static <T> void notEmpty(final T[] obj, final String message) {
+		if (CollectionUtils.isEmpty(obj)) {
+			throwIAE(message);
+		}
+	}
+
+	/**
+	 * 数组不能为空，否则抛出异常
+	 *
+	 * @param obj      目标对象
+	 * @param supplier EX Supplier
+	 * @param <T>      泛型
+	 * @param <EX>     泛型
+	 */
+	public static <T, EX extends RuntimeException> void notEmpty(final T[] obj, final Supplier<EX> supplier) {
+		if (CollectionUtils.isEmpty(obj)) {
+			throw supplier.get();
+		}
+	}
+
+
+	/**
+	 * 数组必须为空，否则抛出异常
+	 *
+	 * @param obj 目标对象
+	 * @param <T> 泛型
+	 */
+	public static <T> void isEmpty(final T[] obj) {
+		if (CollectionUtils.nonEmpty(obj)) {
+			throwIAE("The array is not empty , but it is required empty.");
+		}
+	}
+
+	/**
+	 * 数组必须为空，否则抛出异常
+	 *
+	 * @param obj     目标对象
+	 * @param message 提示信息
+	 * @param <T>     泛型
+	 */
+	public static <T> void isEmpty(final T[] obj, final String message) {
+		if (CollectionUtils.nonEmpty(obj)) {
+			throwIAE(message);
+		}
+	}
+
+	/**
+	 * 数组必须为空，否则抛出异常
+	 *
+	 * @param obj      目标对象
+	 * @param supplier EX Supplier
+	 * @param <T>      泛型
+	 * @param <EX>     泛型
+	 */
+	public static <T, EX extends RuntimeException> void isEmpty(final T[] obj, final Supplier<EX> supplier) {
+		if (CollectionUtils.nonEmpty(obj)) {
+			throw supplier.get();
+		}
+	}
+
+	/**
+	 * 集合不能为空，否则抛出异常
+	 *
+	 * @param collection 目标集合
+	 * @param <T>        泛型
+	 */
+	public static <T extends Collection<?>> void notEmpty(final T collection) {
+		if (CollectionUtils.isEmpty(collection)) {
+			throwIAE("The collection is empty , but it is required not empty.");
+		}
+	}
+
+	/**
+	 * 集合不能为空，否则抛出异常
+	 *
+	 * @param collection 目标集合
+	 * @param message    提示信息
+	 * @param <T>        泛型
+	 */
+	public static <T extends Collection<?>> void notEmpty(final T collection, final String message) {
+		if (CollectionUtils.isEmpty(collection)) {
+			throwIAE(message);
+		}
+	}
+
+	/**
+	 * 集合不能为空，否则抛出异常
+	 *
+	 * @param collection 目标集合
+	 * @param supplier   EX Supplier
+	 * @param <T>        泛型
+	 * @param <EX>       泛型
+	 */
+	public static <T extends Collection<?>, EX extends RuntimeException> void notEmpty(final T collection, final Supplier<EX> supplier) {
+		if (CollectionUtils.isEmpty(collection)) {
+			throw supplier.get();
+		}
+	}
+
+	/**
+	 * 集合必须为空或者 null，否则抛出异常
+	 *
+	 * @param collection 目标集合
+	 * @param <T>        泛型
+	 */
+	public static <T extends Collection<?>> void isEmpty(final T collection) {
+		if (CollectionUtils.nonEmpty(collection)) {
+			throwIAE("The collection is not empty , but it is required empty.");
+		}
+	}
+
+
+	/**
+	 * 集合必须为空或者 null，否则抛出异常
+	 *
+	 * @param collection 目标集合
+	 * @param message    提示信息
+	 * @param <T>        泛型
+	 */
+	public static <T extends Collection<?>> void isEmpty(final T collection, final String message) {
+		if (CollectionUtils.nonEmpty(collection)) {
+			throwIAE(message);
+		}
+	}
+
+	/**
+	 * 集合必须为空或者 null，否则抛出异常
+	 *
+	 * @param collection 目标集合
+	 * @param supplier   EX Supplier
+	 * @param <T>        泛型
+	 * @param <EX>       泛型
+	 */
+	public static <T extends Collection<?>, EX extends RuntimeException> void isEmpty(final T collection, final Supplier<EX> supplier) {
+		if (CollectionUtils.nonEmpty(collection)) {
+			throw supplier.get();
+		}
+	}
+
+	/**
+	 * map集合不能为空，否则抛出异常
+	 *
+	 * @param map 目标集合
+	 * @param <T> 泛型
+	 */
+	public static <T extends Map<?, ?>> void notEmpty(final T map) {
+		if (MapUtils.isEmpty(map)) {
+			throwIAE("The map is empty , but it is required not empty.");
+		}
+	}
+
+	/**
+	 * map集合不能为空，否则抛出异常
+	 *
+	 * @param map     目标集合
+	 * @param message 提示信息
+	 * @param <T>     泛型
+	 */
+	public static <T extends Map<?, ?>> void notEmpty(final T map, final String message) {
+		if (MapUtils.isEmpty(map)) {
+			throwIAE(message);
+		}
+	}
+
+	/**
+	 * map集合不能为空，否则抛出异常
+	 *
+	 * @param map      目标集合
+	 * @param supplier EX Supplier
+	 * @param <T>      泛型
+	 * @param <EX>     泛型
+	 */
+	public static <T extends Map<?, ?>, EX extends RuntimeException> void notEmpty(final T map, final Supplier<EX> supplier) {
+		if (MapUtils.isEmpty(map)) {
+			throw supplier.get();
+		}
+	}
+
+	/**
+	 * map集合必须为空，否则抛出异常
+	 *
+	 * @param map 目标集合
+	 * @param <T> 泛型
+	 */
+	public static <T extends Map<?, ?>> void isEmpty(final T map) {
+		if (MapUtils.nonEmpty(map)) {
+			throwIAE("The map is not empty , but it is required empty.");
+		}
+	}
+
+	/**
+	 * map集合必须为空，否则抛出异常
+	 *
+	 * @param map     目标集合
+	 * @param message 提示信息
+	 * @param <T>     泛型
+	 */
+	public static <T extends Map<?, ?>> void isEmpty(final T map, final String message) {
+		if (MapUtils.nonEmpty(map)) {
+			throwIAE(message);
+		}
+	}
+
+	/**
+	 * map集合必须为空，否则抛出异常
+	 *
+	 * @param map      目标集合
+	 * @param supplier EX Supplier
+	 * @param <T>      泛型
+	 * @param <EX>     泛型
+	 */
+	public static <T extends Map<?, ?>, EX extends RuntimeException> void isEmpty(final T map, final Supplier<EX> supplier) {
+		if (MapUtils.nonEmpty(map)) {
+			throw supplier.get();
+		}
+	}
 }
--- armguard-common/src/main/java/vip/openpark/armguard/common/util/EnumUtils.java
+++ armguard-common/src/main/java/vip/openpark/armguard/common/util/EnumUtils.java
@@ -1,6 +1,5 @@
 package vip.openpark.armguard.common.util;
 
-import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.Arrays;
 
@@ -14,185 +13,168 @@ import java.util.Arrays;
  * </body>
  *
  * @author anthony
- * @version 2023/6/13
+ * @version 2023/11/28 16:19:12
  */
 public class EnumUtils {
 
-    private EnumUtils() {
-    }
+	private EnumUtils() {
+	}
 
-    /**
-     * 校验枚举值是否合法
-     *
-     * @param cls     枚举 Class
-     * @param codeVal 对应的 code 值
-     * @return true 表示合法，false 表示不合法
-     */
-    public static <E extends Enum<E>> boolean checkLegal(final Class<E> cls, final Object codeVal) {
-        return checkLegal(cls, "getCode", codeVal);
-    }
+	/**
+	 * 校验枚举值是否合法
+	 *
+	 * @param cls     枚举 Class
+	 * @param codeVal 对应的 code 值
+	 * @return true 表示合法，false 表示不合法
+	 */
+	public static <E extends Enum<E>> boolean checkLegal(final Class<E> cls, final Object codeVal) {
+		return checkLegal(cls, "getCode", codeVal);
+	}
 
-    /**
-     * 校验枚举值是否合法
-     *
-     * @param cls               枚举 Class
-     * @param getCodeMethodName 获取 code 方法
-     * @param codeVal           对应的 code 值
-     * @return true 表示合法，false 表示不合法
-     */
-    public static <E extends Enum<E>> boolean checkLegal(final Class<E> cls,
-                                                         final String getCodeMethodName,
-                                                         final Object codeVal) {
-        if (null == cls || !cls.isEnum() ||
-                null == getCodeMethodName || getCodeMethodName.isEmpty()) {
-            return false;
-        }
+	/**
+	 * 校验枚举值是否合法
+	 *
+	 * @param cls               枚举 Class
+	 * @param getCodeMethodName 获取 code 方法
+	 * @param codeVal           对应的 code 值
+	 * @return true 表示合法，false 表示不合法
+	 */
+	public static <E extends Enum<E>> boolean checkLegal(final Class<E> cls,
+	                                                     final String getCodeMethodName,
+	                                                     final Object codeVal) {
+		if (null == cls || !cls.isEnum() ||
+			null == getCodeMethodName || getCodeMethodName.isEmpty()) {
+			return false;
+		}
 
-        try {
-            Method method = cls.getMethod(getCodeMethodName);
+		E anEnum = getEnum(cls, getCodeMethodName, codeVal);
 
-            return Arrays.stream(cls.getEnumConstants())
-                    .anyMatch(anEnum -> {
-                        try {
-                            // 获取每个枚举对应方法的值
-                            Object enumCodeVal = method.invoke(anEnum);
+		return null != anEnum;
+	}
 
-                            return (null == enumCodeVal && null == codeVal) ||
-                                    (null != enumCodeVal && enumCodeVal.equals(codeVal));
-                        } catch (IllegalAccessException | InvocationTargetException e) {
-                            // Consider printing the log
-                            return false;
-                        }
-                    });
-        } catch (Exception e) {
-            // Consider printing the log
-            return false;
-        }
-    }
+	/**
+	 * 利用反射原理获取 codeVal 对应的其他字段属性值（例如描述相关）
+	 *
+	 * @param cls     枚举 Class
+	 * @param codeVal 对应的 code 值
+	 * @return 返回字符串对象
+	 */
+	public static <E extends Enum<E>> String getStr(final Class<E> cls,
+	                                                final Object codeVal) {
+		return getVal(cls, codeVal, String.class);
+	}
 
-    /**
-     * 利用反射原理获取 codeVal 对应的其他字段属性值（例如描述相关）
-     *
-     * @param cls     枚举 Class
-     * @param codeVal 对应的 code 值
-     * @return 返回字符串对象
-     */
-    public static <E extends Enum<E>> String getStr(final Class<E> cls,
-                                                    final Object codeVal) {
-        return getVal(cls, codeVal, String.class);
-    }
+	/**
+	 * 利用反射原理获取 codeVal 对应的其他字段属性值（例如描述相关）
+	 *
+	 * @param cls       枚举 Class
+	 * @param codeVal   对应的 code 值
+	 * @param resultCls 返回结果对象
+	 * @param <E>       枚举泛型
+	 * @param <R>       结果泛型
+	 * @return 返回结果对象
+	 */
+	public static <E extends Enum<E>, R> R getVal(final Class<E> cls, final Object codeVal, final Class<R> resultCls) {
+		return getVal(cls, "getCode", codeVal, "getDesc", resultCls);
+	}
 
-    /**
-     * 利用反射原理获取 codeVal 对应的其他字段属性值（例如描述相关）
-     *
-     * @param cls       枚举 Class
-     * @param codeVal   对应的 code 值
-     * @param resultCls 返回结果对象
-     * @param <E>       枚举泛型
-     * @param <R>       结果泛型
-     * @return 返回结果对象
-     */
-    public static <E extends Enum<E>, R> R getVal(final Class<E> cls, final Object codeVal, final Class<R> resultCls) {
-        return getVal(cls, "getCode", codeVal, "getDesc", resultCls);
-    }
+	/**
+	 * 利用反射原理获取 codeVal 对应的其他字段属性值（例如描述相关）
+	 *
+	 * @param cls               枚举 Class
+	 * @param getCodeMethodName 获取 code 的方法
+	 * @param codeVal           对应的 code 值
+	 * @param getDescMethodName 获取描述的方法
+	 * @param resultCls         结果的 Class 对象
+	 * @param <E>               枚举泛型
+	 * @param <R>               结果的泛型
+	 * @return 返回结果对象
+	 */
+	public static <E extends Enum<E>, R> R getVal(final Class<E> cls,
+	                                              final String getCodeMethodName,
+	                                              final Object codeVal,
+	                                              final String getDescMethodName,
+	                                              final Class<R> resultCls) {
+		// 判断 cls 必须是枚举类型
+		if (null == cls || !cls.isEnum() ||
+			null == getCodeMethodName || getCodeMethodName.isEmpty() ||
+			null == getDescMethodName || getDescMethodName.isEmpty() ||
+			null == resultCls) {
+			return null;
+		}
 
-    /**
-     * 利用反射原理获取 codeVal 对应的其他字段属性值（例如描述相关）
-     *
-     * @param cls               枚举 Class
-     * @param getCodeMethodName 获取 code 的方法
-     * @param codeVal           对应的 code 值
-     * @param getDescMethodName 获取描述的方法
-     * @param resultCls         结果的 Class 对象
-     * @param <E>               枚举泛型
-     * @param <R>               结果的泛型
-     * @return 返回结果对象
-     */
-    public static <E extends Enum<E>, R> R getVal(final Class<E> cls,
-                                                  final String getCodeMethodName,
-                                                  final Object codeVal,
-                                                  final String getDescMethodName,
-                                                  final Class<R> resultCls) {
-        // 判断 cls 必须是枚举类型
-        if (null == cls || !cls.isEnum() ||
-                null == getCodeMethodName || getCodeMethodName.isEmpty() ||
-                null == getDescMethodName || getDescMethodName.isEmpty() ||
-                null == resultCls) {
-            return null;
-        }
+		try {
+			E targetEnum = getEnum(cls, getCodeMethodName, codeVal);
+			if (null == targetEnum) {
+				return null;
+			}
 
-        try {
-            E targetEnum = getEnum(cls, getCodeMethodName, codeVal);
-            if (null == targetEnum) {
-                return null;
-            }
+			// 获取枚举对应的描述值
+			Method descMethod = cls.getMethod(getDescMethodName);
+			Object obj = descMethod.invoke(targetEnum);
+			if (null != obj && resultCls.equals(obj.getClass())) {
+				// 将对象转换为此Class对象表示的类或接口
+				return resultCls.cast(obj);
+			}
 
-            // 获取枚举对应的描述值
-            Method descMethod = cls.getMethod(getDescMethodName);
-            Object obj = descMethod.invoke(targetEnum);
-            if (null != obj && resultCls.equals(obj.getClass())) {
-                // 将对象转换为此Class对象表示的类或接口
-                return resultCls.cast(obj);
-            }
-
-            // 无法转换对象
-            return null;
-        } catch (Exception e) {
-            // Consider printing the log
-            return null;
-        }
-    }
+			// 无法转换对象
+			return null;
+		} catch (Exception e) {
+			// Consider printing the log
+			return null;
+		}
+	}
 
 
-    /**
-     * 获取枚举
-     *
-     * @param cls     枚举 Class
-     * @param codeVal 对应的 code 值
-     * @param <E>     结果的泛型
-     * @return 结果枚举
-     */
-    public static <E extends Enum<E>> E getEnum(final Class<E> cls,
-                                                final Object codeVal) {
-        return getEnum(cls, "getCode", codeVal);
-    }
+	/**
+	 * 获取枚举
+	 *
+	 * @param cls     枚举 Class
+	 * @param codeVal 对应的 code 值
+	 * @param <E>     结果的泛型
+	 * @return 结果枚举
+	 */
+	public static <E extends Enum<E>> E getEnum(final Class<E> cls,
+	                                            final Object codeVal) {
+		return getEnum(cls, "getCode", codeVal);
+	}
 
-    /**
-     * 获取枚举
-     *
-     * @param cls     枚举 Class
-     * @param codeVal 对应的 code 值
-     * @param <E>     结果的泛型
-     * @return 结果枚举
-     */
-    public static <E extends Enum<E>> E getEnum(final Class<E> cls,
-                                                final String getCodeMethodName,
-                                                final Object codeVal) {
-        // 判断 cls 必须是枚举类型
-        if (null == cls || !cls.isEnum() ||
-                null == getCodeMethodName || getCodeMethodName.isEmpty()) {
-            return null;
-        }
+	/**
+	 * 获取枚举
+	 *
+	 * @param cls     枚举 Class
+	 * @param codeVal 对应的 code 值
+	 * @param <E>     结果的泛型
+	 * @return 结果枚举
+	 */
+	public static <E extends Enum<E>> E getEnum(final Class<E> cls,
+	                                            final String getCodeMethodName,
+	                                            final Object codeVal) {
+		// 判断 cls 必须是枚举类型
+		if (null == cls || !cls.isEnum() ||
+			null == getCodeMethodName || getCodeMethodName.isEmpty()) {
+			return null;
+		}
 
-        try {
-            Method codeMethod = cls.getMethod(getCodeMethodName);
+		try {
+			Method codeMethod = cls.getMethod(getCodeMethodName);
 
-            return Arrays.stream(cls.getEnumConstants()) // 获取所有枚举值
-                    .filter(anEnum -> {
-                        try {
-                            Object enumCodeVal = codeMethod.invoke(anEnum);
-                            return (null == enumCodeVal && null == codeVal) ||
-                                    (null != enumCodeVal && enumCodeVal.equals(codeVal));
-                        } catch (Exception e) {
-                            // Consider printing the log
-                            return false;
-                        }
-                    })
-                    .findFirst()
-                    .orElse(null);
-        } catch (Exception e) {
-            // Consider printing the log
-            return null;
-        }
-    }
+			return Arrays.stream(cls.getEnumConstants()) // 获取所有枚举值
+				.filter(anEnum -> {
+					try {
+						Object enumCodeVal = codeMethod.invoke(anEnum);
+						return (null == enumCodeVal && null == codeVal) ||
+							(null != enumCodeVal && enumCodeVal.equals(codeVal));
+					} catch (Exception e) {
+						// Consider printing the log
+						return false;
+					}
+				})
+				.findFirst()
+				.orElse(null);
+		} catch (Exception e) {
+			// Consider printing the log
+			return null;
+		}
+	}
 }
