65b0ccea651c85fe59dac51607835502323f7ca9
anthony <anthony1949@zto.com>
2023-12-05 18:40:15 +0800

    feature：添加 DateTimeFormatter 常量

--- armguard-common/src/main/java/vip/openpark/armguard/common/constant/DatePatterPool.java
+++ armguard-common/src/main/java/vip/openpark/armguard/common/constant/DatePatterPool.java
@@ -1,21 +1,27 @@
 package vip.openpark.armguard.common.constant;
 
+import java.time.format.DateTimeFormatter;
+
 /**
  * @author anthony
  * @version 2023/6/13
  */
 public interface DatePatterPool {
-    String DATE_0 = "yyyy-MM-dd";
-    String DATE_1 = "yyyy年MM月dd日";
+	String DATE_0 = "yyyy-MM-dd";
+	String DATE_1 = "yyyy年MM月dd日";
 
-    String TIME_0 = "HH:mm:ss";
-    String TIME_1 = "HH时mm分ss秒";
+	String TIME_0 = "HH:mm:ss";
+	String TIME_1 = "HH时mm分ss秒";
 
-    String DATE_TIME_0 = "yyyy-MM-dd HH:mm:ss";
-    String DATE_TIME_1 = "yyyy年MM月dd日 HH时mm分ss秒";
+	String DATE_TIME_0 = "yyyy-MM-dd HH:mm:ss";
+	String DATE_TIME_1 = "yyyy年MM月dd日 HH时mm分ss秒";
 
-    String DATE_TIME_UTC_0 = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'";
-    String DATE_TIME_UTC_1 = "yyyy-MM-dd'T'HH:mm:ss.SSSZ";
-    String DATE_TIME_UTC_2 = "yyyy-MM-dd'T'HH:mm:ss.SSS";
-    String DATE_TIME_UTC_3 = "yyyy-MM-dd'T'HH:mm:ss";
+	String DATE_TIME_UTC_0 = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'";
+	String DATE_TIME_UTC_1 = "yyyy-MM-dd'T'HH:mm:ss.SSSZ";
+	String DATE_TIME_UTC_2 = "yyyy-MM-dd'T'HH:mm:ss.SSS";
+	String DATE_TIME_UTC_3 = "yyyy-MM-dd'T'HH:mm:ss";
+
+	DateTimeFormatter DATE_FORMATTER_0 = DateTimeFormatter.ofPattern(DATE_0);
+	DateTimeFormatter TIME_FORMATTER_0 = DateTimeFormatter.ofPattern(TIME_0);
+	DateTimeFormatter DATE_TIME_FORMATTER_0 = DateTimeFormatter.ofPattern(DATE_TIME_0);
 }
