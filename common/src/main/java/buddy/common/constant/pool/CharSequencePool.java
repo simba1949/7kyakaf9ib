0217ce7a198d98145c727c3cb77ef8ab20dbbf9d
anthony <anthony1949@zto.com>
2024-11-07 17:42:39 +0800

    feature：字符常量池 feature：元组

--- common/src/main/java/buddy/common/constant/pool/CharSequencePool.java
+++ common/src/main/java/buddy/common/constant/pool/CharSequencePool.java
@@ -1,23 +1,53 @@
 package buddy.common.constant.pool;
 
 /**
- * 字符串和字符常量池
+ * 字符/字符串相关常量池
  *
  * @author anthony
- * @version 2024/09/11
+ * @version 2024/11/07
  * @since 2023/10/19 13:56
  */
 public interface CharSequencePool {
+	char DOT = '.';
+	char COLON = ':';
 	char COMMA = ','; // 半角逗号
 	char SEMICOLON = ';'; // 半角分号
-	char EXCLAMATION_MARK = '!';  // 半角感叹号
-	char DOT = '.'; // 半角点
-	char SPACE = ' '; // 半角空格
+	char SINGLE_QUOTE = '\'';
+	char DOUBLE_QUOTE = '"';
 
+	char LEFT_BRACKET = '(';
+	char RIGHT_BRACKET = ')';
+	char LEFT_BRACE = '{';
+	char RIGHT_BRACE = '}';
+	char LEFT_SQUARE_BRACKET = '[';
+	char RIGHT_SQUARE_BRACKET = ']';
+	char LEFT_ANGLE_BRACKET = '<';
+	char RIGHT_ANGLE_BRACKET = '>';
+
+	char SPACE = ' '; // 半角空格
+	char TAB = '\t';
+	char CR = '\r';
+	char LF = '\n';
+	char CR_LF = CR + LF;
+
+	char SLASH = '/';
+	char BACKSLASH = '\\';
 	char DASHED_LINE = '-'; // 中划线
 	char UNDERSCORE = '_'; // 下划线
 
-	char AMPERSAND = '&';
+	char QUESTION_MARK = '?';
+	char EXCLAMATION_MARK = '!';  // 半角感叹号
+
 	char AT = '@';
-	char PER_CENT = '%';
+	char AMPERSAND = '&';
+	char POUND = '#';
+	char DOLLAR = '$';
+	char PERCENT = '%';
+	char CARET = '^';
+	char ASTERISK = '*';
+
+	char EQUAL = '=';
+	char PLUS = '+';
+	char MINUS = '-';
+	char DIVIDE = '/';
 }
---
+++ common/src/main/java/buddy/common/tuple/AbstractTuple.java
@@ -0,0 +1,9 @@
+package buddy.common.tuple;
+
+/**
+ * @author anthony
+ * @version 2024-11-07
+ * @since 2024-11-07 15:54
+ */
+public abstract class AbstractTuple {
+}
